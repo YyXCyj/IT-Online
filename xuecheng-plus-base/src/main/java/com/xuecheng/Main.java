2f1ad3d2a2469aabf80af254f27e42e931f4d8db
YyXCyj <2289150297@qq.com>
2023-01-28 11:38:27 +0800

    父工程和基础工程pom文件的提交

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/Main.java
@@ -0,0 +1,11 @@
+package com.xuecheng;
+
+/**
+ * @author yy
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
